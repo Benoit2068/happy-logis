ef74786f32c2f10d49d1e6ac21a4d9bac5bcb7ab
Benjamin Thomas <b.thomas@lynceus-partners.com>
2022-03-17 13:27:08 +0100

    show adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   resources :deliveries do
     resources :loadings do
       resources :materials, only: :create
     end
   end
-end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/my_deliveries', to: 'deliveries#my_deliveries'
+  resources :deliveries
+
+end
