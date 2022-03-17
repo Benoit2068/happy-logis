bb2b7afd784498ecb6d6b289919d30d5a10b83b8
Benjamin Thomas <b.thomas@lynceus-partners.com>
2022-03-17 13:35:18 +0100

    intermediate

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  #devise_scope :user do
-   # root to: "devise/sessions#new"
-  #end
-
-  resources :deliveries do
-    resources :loadings do
-      resources :materials, only: :create
-    end
-  end
+  # root to: 'pages#home'
+    devise_scope :user do
+  root to: "devise/sessions#new"
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/my_deliveries', to: 'deliveries#my_deliveries'
+  resources :deliveries
 end
