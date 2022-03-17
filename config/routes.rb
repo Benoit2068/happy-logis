e2fabcf908f74c702a1c26668c567bb458339f44
Benjamin Thomas <b.thomas@lynceus-partners.com>
2022-03-17 13:36:45 +0100

    intermediate

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "devise/sessions#new"
 end
 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/my_deliveries', to: 'deliveries#my_deliveries'
   resources :deliveries
