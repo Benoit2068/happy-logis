9dec44150b18121412d8c493aa23a69042e41fbf
Benjamin Thomas <b.thomas@lynceus-partners.com>
2022-04-01 15:49:22 +0200

    added buttons and worked on views + new

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -30,7 +30,7 @@ class DeliveriesController < ApplicationController
   def create
     @delivery = Delivery.new(delivery_params)
     @delivery.save
-    redirect_to delivery_path(@delivery)
+    redirect_to deliveries_path
   end
 
   def edit
@@ -38,7 +38,7 @@ class DeliveriesController < ApplicationController
 
   def update
     @delivery.update(delivery_params)
-    redirect_to delivery_path(@delivery)
+    redirect_to deliveries_path
   end
 
   def destroy
@@ -49,7 +49,7 @@ class DeliveriesController < ApplicationController
   private
 
   def delivery_params
-    params.require(:delivery).permit(:name, :startdate, :capacity)
+    params.require(:delivery).permit(:name, :startdate, :capacity, :user_id)
   end
 
   def set_delivery
