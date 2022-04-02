1604958da8c359dbfabb86d3c95f85b1858bfacb
benoit sommariva <benoitsommariva@gmail.com>
2022-04-02 12:20:31 +0200

    boubacar marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,61 +1,6 @@
 
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import LoaderOptionsPlugin from 'webpack/lib/LoaderOptionsPlugin';
-
-
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
-  };
-
-  const addMarkers = (map, markers) => {
-    map.addControl(new mapboxgl.FullscreenControl());
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_marker);
-
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
-        .addTo(map);
-      });
-    };
-
-    if (mapElement) { // only build a map if there's a div#map to inject into
-      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10'
-      });
-
-      const markers = JSON.parse(mapElement.dataset.markers);
-
-      fitMapToMarkers(map, markers);
-      addMarkers(map, markers);
-  }
-};
-
-export { initMapbox };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -65,36 +10,70 @@ export { initMapbox };
 //   const fitMapToMarkers = (map, markers) => {
 //     const bounds = new mapboxgl.LngLatBounds();
 //     markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-//     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+//     map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
 //   };
-//   if (mapElement) { // only build a map if there's a div#map to inject into
-//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v10',
-//       // center: [11.255, 43.77], // starting position
-//       // zoom: 13 // starting zoom
-//     });
-//     map.addControl(new mapboxgl.FullscreenControl());
-//     const markers = JSON.parse(mapElement.dataset.markers);
-//     markers.forEach((marker) => {
-//     for (const marker of markers) {
-//       const el = document.createElement('div');
-//       el.className = 'marker';
-//       new mapboxgl.Marker(el)
-//         .setLngLat([marker.lng, marker.lat])
-//         .setPopup(
-//             new mapboxgl.Popup({ offset: 25 }) // add popups
-//               .setHTML(
 
-//                 `<h3>${marker.name}</h3>
-//                 <p>${marker.address}</p>`
-//                 // marker.element.forEach((element) => {
-//                 //   `${element.quantity}`;
-//                 // });
-//               )
-//           )
+//   const addMarkers = (map, markers) => {
+//     map.addControl(new mapboxgl.FullscreenControl());
+//     markers.forEach((marker) => {
+//       const popup = new mapboxgl.Popup().setHTML(marker.info_marker);
+
+//       new mapboxgl.Marker()
+//         .setLngLat([marker.lng, marker.lat])
+//         .setPopup(popup)
 //         .addTo(map);
-//     fitMapToMarkers(map, markers);
+//       });
+//     };
+
+//     if (mapElement) { // only build a map if there's a div#map to inject into
+//       mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+//       const map = new mapboxgl.Map({
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/streets-v10'
+//       });
+
+//       const markers = JSON.parse(mapElement.dataset.markers);
+
+//       fitMapToMarkers(map, markers);
+//       addMarkers(map, markers);
 //   }
+// };
+
 // export { initMapbox };
+
+// -------------------------------------------------------------
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [11.255, 43.77], // starting position
+      zoom: 13 // starting zoom
+    });
+    map.addControl(new mapboxgl.FullscreenControl());
+    const markers = JSON.parse(mapElement.dataset.markers);
+
+    for (const marker of markers) {
+      // Create a DOM element for each marker.
+      const popup = new mapboxgl.Popup().setHTML(marker.info_marker);
+      const el = document.createElement('div');
+      el.className = 'marker';
+      // Add markers to the map.
+      new mapboxgl.Marker(el)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(map);
+    }
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
+    fitMapToMarkers(map, markers);
+    //}
+  }
+};
+export { initMapbox };
