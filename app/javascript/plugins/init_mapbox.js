1dc179133c707fda64f83ad64aa380c7d5b23047
benoit sommariva <benoitsommariva@gmail.com>
2022-04-05 13:32:25 +0200

    cleaning init_mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,7 +2,7 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-
+// -------------------------Boubacar's code------------------------------------------
 
 // const initMapbox = () => {
 //   const mapElement = document.getElementById('map');
@@ -41,46 +41,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 // export { initMapbox };
 
-// -------------------------------------------------------------
+// ---------------------------------Ben's Code-----------------------------------------
 
-// const initMapbox = () => {
-//   const mapElement = document.getElementById('map');
-//   if (mapElement) { // only build a map if there's a div#map to inject into
-//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v10',
-//       center: [11.255, 43.77], // starting position
-//       zoom: 13 // starting zoom
-//     });
-//     map.addControl(new mapboxgl.FullscreenControl());
-//     const markers = JSON.parse(mapElement.dataset.markers);
-
-//     for (const marker of markers) {
-//       // Create a DOM element for each marker.
-//       const popup = new mapboxgl.Popup().setHTML(marker.info_marker);
-//       const el = document.createElement('div');
-//       el.className = 'marker';
-//       // el.innerHTML = '<span><b>' + (i + 1) + '</b></span>'
-//       // Add markers to the map.
-//       new mapboxgl.Marker(el)
-//         .setLngLat([marker.lng, marker.lat])
-//         .setPopup(popup)
-//         .addTo(map);
-//     }
-//     const fitMapToMarkers = (map, markers) => {
-//       const bounds = new mapboxgl.LngLatBounds();
-//       markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-//       map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-//     };
-//     fitMapToMarkers(map, markers);
-//     //}
-//   }
-// };
-// export { initMapbox };
-
-
-// ---------------Test_number_in marker--------------------------
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -122,5 +84,4 @@ const initMapbox = () => {
       addMarkers(map, markers);
   }
 };
-
 export { initMapbox };
