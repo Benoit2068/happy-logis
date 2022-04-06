c4f2a0c2aa3a284dcd29803a10796216a8433668
benoit sommariva <benoitsommariva@gmail.com>
2022-04-06 09:22:48 +0200

    seed for manu

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ fruit_juice = Material.create({ name: 'Fruit juice' })
 rhum = Material.create({ name: 'Rhum' })
 coca_cola = Material.create({ name: 'Coca Cola' })
 chips = Material.create({ name: 'Chips' })
-
+flour = Material.create({ name: 'Flour' })
 
 puts "material #{meat.name} created"
 puts "material #{wheat.name} created"
@@ -46,15 +46,14 @@ puts "material #{fruit_juice.name} created"
 puts "material #{rhum.name} created"
 puts "material #{coca_cola.name} created"
 puts "material #{chips.name} created"
+puts "material #{flour.name} created"
 
-
-delivery_zurich = Delivery.new({ name: "Zurich", startdate: Date.today + 1, capacity: 33, finished: false})
+delivery_zurich = Delivery.new({ name: "Zurich", startdate: Date.today - 1 , capacity: 33, finished: false})
 delivery_zurich.user = manu
 delivery_zurich.save
 
-delivery_lausanne = Delivery.new({ name: "Lausanne", startdate: Date.today - 1, capacity: 33, finished: true})
-delivery_lausanne.user = manu
-delivery_lausanne.save
+delivery_lausanne = Delivery.new({ name: "Lausanne", startdate: Date.today + 1, capacity: 33, finished: true})
+delivery_lausanne.user = john
 
 
 delivery_geneve = Delivery.new({ name: "Genève", startdate: Date.today, capacity: 33, finished: false})
@@ -69,8 +68,8 @@ delivery_geneve_2 = Delivery.new({ name: "Genève", startdate: Date.today + 3, c
 delivery_geneve_2.user = manu
 delivery_geneve_2.save
 
-delivery_sierre_2 = Delivery.new({ name: "Sierre", startdate: Date.today, capacity: 33, finished: false})
-delivery_sierre_2.user = john
+delivery_sierre_2 = Delivery.new({ name: "Sierre", startdate: Date.today + 1, capacity: 33, finished: false})
+delivery_sierre_2.user = manu
 delivery_sierre_2.save
 
 
@@ -82,14 +81,14 @@ puts "delivery #{delivery_sierre_2.name} create"
 puts "delivery #{delivery_geneve_2.name} create"
 
 
-load_1 = Loading.create({date: Date.today + 1, name: "Moulin de la Pallanterie SA", address: "Rte de Thonon 154, 1222 Vésenaz", done: false, delivery_id: delivery_zurich.id})
-load_2 = Loading.create({date: Date.today + 1, name: "Singh Handel", address: "Hofstattstrasse 4A, 4552 Derendingen", done: false, delivery_id: delivery_zurich.id})
-load_3 = Loading.create({date: Date.today + 1, name: "Beerhub GmbH", address: "Zwickystrasse 2, 8304 Wallisellen", done: false, delivery_id: delivery_zurich.id})
-load_4 = Loading.create({date: Date.today + 1, name: "Centrale Coop Schafisheim", address: "Rupperswilerstrasse 2, 5503 Schafisheim", done: false, delivery_id: delivery_zurich.id})
-load_5 = Loading.create({date: Date.today + 1, name: "Centrale Coop Aclens", address: "Route de la Plaine, 1123 Aclens", done: false, delivery_id: delivery_zurich.id})
+load_1 = Loading.create({date: Date.today - 1, name: "Moulin de la Pallanterie SA", address: "Rte de Thonon 154, 1222 Vésenaz", done: true, delivery_id: delivery_zurich.id})
+load_2 = Loading.create({date: Date.today - 1, name: "Stettler Comestibles SA", address: "Rte de Carrouge 12, 1509 Vucherens", done: true, delivery_id: delivery_zurich.id})
+load_3 = Loading.create({date: Date.today - 1, name: "Beerhub GmbH", address: "Zwickystrasse 2, 8304 Wallisellen", done: true, delivery_id: delivery_zurich.id})
+load_4 = Loading.create({date: Date.today - 1, name: "Centrale Coop Schafisheim", address: "Rupperswilerstrasse 2, 5503 Schafisheim", done: true, delivery_id: delivery_zurich.id})
+load_5 = Loading.create({date: Date.today - 1, name: "Centrale Coop Aclens", address: "Route de la Plaine, 1123 Aclens", done: false, delivery_id: delivery_zurich.id})
 load_6 = Loading.create({date: Date.today, name: "Boissons Liechti SA", address: "chemin de la Milice 20, 1228 Plan-les-Ouates", done: true, delivery_id: delivery_geneve.id})
 load_7 = Loading.create({date: Date.today, name: "Prodegua Satigny", address: "rue De-Turrettin 2, 1242 Satigny", done: false, delivery_id: delivery_geneve.id})
-# load_8 = Loading.create({date: Date.today, name: "Le Wagon Lausanne", address: "Rue de Lausanne 64, 1020 Renens", done: false, delivery_id: delivery_geneve.id})
+load_8 = Loading.create({date: Date.today, name: "Le Saint-Jean Bar et Restaurant", address: "Rue Saint-Jean 22, 1260 Nyon", done: false, delivery_id: delivery_geneve.id})
 
 puts "load #{load_1.address} create"
 puts "load #{load_2.address} create"
@@ -98,21 +97,25 @@ puts "load #{load_4.address} create"
 puts "load #{load_5.address} create"
 puts "load #{load_6.address} create"
 
-loading_material_1 = Loadingmaterial.create({ loading_id: load_1.id, material_id: meat.id, quantity: 15 })
+loading_material_1 = Loadingmaterial.create({ loading_id: load_1.id, material_id: flour.id, quantity: 15 })
 loading_material_2 = Loadingmaterial.create({ loading_id: load_2.id, material_id: egg.id, quantity: 10 })
 loading_material_3 = Loadingmaterial.create({ loading_id: load_3.id, material_id: beer.id, quantity: 8 })
-loading_material_4 = Loadingmaterial.create({ loading_id: load_4.id, material_id: wheat.id, quantity: -10 })
-loading_material_5 = Loadingmaterial.create({ loading_id: load_5.id, material_id: milk.id, quantity: -8 })
-loading_material_6 = Loadingmaterial.create({ loading_id: load_3.id, material_id: wine.id, quantity: -15 })
+loading_material_4 = Loadingmaterial.create({ loading_id: load_4.id, material_id: flour.id, quantity: -5 })
+loading_material_16 = Loadingmaterial.create({ loading_id: load_4.id, material_id: egg.id, quantity: -3 })
+loading_material_17 = Loadingmaterial.create({ loading_id: load_5.id, material_id: flour.id, quantity: -10 })
+loading_material_18 = Loadingmaterial.create({ loading_id: load_5.id, material_id: egg.id, quantity: -7 })
+loading_material_19 = Loadingmaterial.create({ loading_id: load_5.id, material_id: beer.id, quantity: -8 })
+
+# loading_material_6 = Loadingmaterial.create({ loading_id: load_3.id, material_id: wine.id, quantity: -15 })
 loading_material_7 = Loadingmaterial.create({ loading_id: load_6.id, material_id: beer.id, quantity: 10 })
 loading_material_8 = Loadingmaterial.create({ loading_id: load_6.id, material_id: fruit_juice.id, quantity: 5 })
 loading_material_9 = Loadingmaterial.create({ loading_id: load_6.id, material_id: coca_cola.id, quantity: 7 })
 loading_material_10 = Loadingmaterial.create({ loading_id: load_7.id, material_id: beer.id, quantity: -2 })
 loading_material_11 = Loadingmaterial.create({ loading_id: load_7.id, material_id: chips.id, quantity: 5 })
-# loading_material_12 = Loadingmaterial.create({ loading_id: load_8.id, material_id: beer.id, quantity: -8 })
-# loading_material_13 = Loadingmaterial.create({ loading_id: load_8.id, material_id: fruit_juice.id, quantity: -5 })
-# loading_material_14 = Loadingmaterial.create({ loading_id: load_8.id, material_id: coca_cola.id, quantity: -7 })
-# loading_material_15 = Loadingmaterial.create({ loading_id: load_8.id, material_id: chips.id, quantity: -5 })
+loading_material_12 = Loadingmaterial.create({ loading_id: load_8.id, material_id: beer.id, quantity: -5 })
+loading_material_13 = Loadingmaterial.create({ loading_id: load_8.id, material_id: fruit_juice.id, quantity: -2 })
+loading_material_14 = Loadingmaterial.create({ loading_id: load_8.id, material_id: coca_cola.id, quantity: -4 })
+loading_material_15 = Loadingmaterial.create({ loading_id: load_8.id, material_id: chips.id, quantity: -3 })
 
 
 
@@ -120,16 +123,20 @@ puts "loadingmaterial #{loading_material_1.material.name}: #{loading_material_1.
 puts "loadingmaterial #{loading_material_2.material.name}: #{loading_material_2.quantity}  created"
 puts "loadingmaterial #{loading_material_3.material.name}: #{loading_material_3.quantity}  created"
 puts "unloadingmaterial #{loading_material_4.material.name}: #{loading_material_4.quantity}  created"
-puts "unloadingmaterial #{loading_material_5.material.name}: #{loading_material_5.quantity}  created"
-puts "unloadingmaterial #{loading_material_6.material.name}: #{loading_material_6.quantity}  created"
+# puts "unloadingmaterial #{loading_material_5.material.name}: #{loading_material_5.quantity}  created"
+# puts "unloadingmaterial #{loading_material_6.material.name}: #{loading_material_6.quantity}  created"
 puts "unloadingmaterial #{loading_material_7.material.name}: #{loading_material_7.quantity}  created"
 puts "unloadingmaterial #{loading_material_8.material.name}: #{loading_material_8.quantity}  created"
 puts "unloadingmaterial #{loading_material_9.material.name}: #{loading_material_9.quantity}  created"
 puts "unloadingmaterial #{loading_material_10.material.name}: #{loading_material_10.quantity}  created"
 puts "unloadingmaterial #{loading_material_11.material.name}: #{loading_material_11.quantity}  created"
-# puts "unloadingmaterial #{loading_material_12.material.name}: #{loading_material_12.quantity}  created"
-# puts "unloadingmaterial #{loading_material_13.material.name}: #{loading_material_13.quantity}  created"
-# puts "unloadingmaterial #{loading_material_14.material.name}: #{loading_material_14.quantity}  created"
-# puts "unloadingmaterial #{loading_material_15.material.name}: #{loading_material_15.quantity}  created"
+puts "unloadingmaterial #{loading_material_12.material.name}: #{loading_material_12.quantity}  created"
+puts "unloadingmaterial #{loading_material_13.material.name}: #{loading_material_13.quantity}  created"
+puts "unloadingmaterial #{loading_material_14.material.name}: #{loading_material_14.quantity}  created"
+puts "unloadingmaterial #{loading_material_15.material.name}: #{loading_material_15.quantity}  created"
+puts "unloadingmaterial #{loading_material_16.material.name}: #{loading_material_16.quantity}  created"
+puts "unloadingmaterial #{loading_material_17.material.name}: #{loading_material_17.quantity}  created"
+puts "unloadingmaterial #{loading_material_18.material.name}: #{loading_material_18.quantity}  created"
+puts "unloadingmaterial #{loading_material_19.material.name}: #{loading_material_19.quantity}  created"
 
 puts "seed created"
